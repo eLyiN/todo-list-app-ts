25ccf84deb9ae0b133fa86044757d01216892288
Adrián Arribas <staffultimaunion@gmail.com>
2024-09-30 22:36:49 +0200

    Refactor todo-controllers to validate description before creating or updating todos

--- backend/src/controllers/todo-controllers.ts
+++ backend/src/controllers/todo-controllers.ts
@@ -12,6 +12,10 @@ export const getAllTodos = async (req: Request, res: Response) => {
 
 export const createNewTodo = async (req: Request, res: Response) => {
   const { description } = req.body;
+  if (!description) {
+    res.status(400).send('Description is required');
+    return;
+  }
   try {
     await createTodo(description);
     res.status(201).send('To-do created');
@@ -23,6 +27,11 @@ export const createNewTodo = async (req: Request, res: Response) => {
 export const updateTodoById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { description } = req.body;
+  if (!description) {
+    res.status(400).send('Description is required');
+    return;
+  }
+  
   try {
     await updateTodo(id, description);
     res.status(200).send('To-do updated');
